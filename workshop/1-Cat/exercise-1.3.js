8b94c42b04b06a9ede436d87af3eecbf9f555936
chris shino <chris.shino@hotmail.com>
2020-10-13 20:46:48 -0400

    done 1.3

--- workshop/1-Cat/exercise-1.3.js
+++ workshop/1-Cat/exercise-1.3.js
@@ -9,6 +9,42 @@
 //    Write methods that will update tiredness, hunger, loneliness, and
 //    happiness.
 
+
+
+class Cat {
+  // Add code here
+  constructor(name, breed) {
+    this.name = name
+    this.breed = breed
+    this.species = "cat";
+    this.tiredness = 50;
+    this.hunger = 100;
+    this.loneliness = 100;
+    this.happiness = 0;
+  }
+
+  sleep = (hours) => {
+    this.tiredness = this.tiredness / (5 * hours)
+    this.happiness += 1
+  }
+
+  eat = (kibble) => {
+    this.hunger = this.hunger - (kibble / 5)
+    this.happiness += 1
+  }
+  
+  play = (minutes) => {
+    this.loneliness = this.loneliness - (minutes*3)
+    this.happiness = this.happiness + (minutes / 2)
+  }
+
+}
+
+let Boots = new Cat ('boots', 'siamese')
+
+console.log(Boots)
+
+
 // HINT: You need to write arrow functions here. () => {}
 // - Write a sleep method that accept hours and reduces tiredness by 5 times
 //   that number. So if the cat has a tiredness of `50`, and sleeps for 10
@@ -22,3 +58,11 @@
 // You decide how much sleep, eat, and play affects your cat's happiness.
 
 // B) call the different methods with appropriate values and then console.log boots to see what happened to the property values.
+
+Boots.sleep(5)
+console.log(Boots.tiredness)
+Boots.eat(20)
+console.log(Boots.hunger)
+Boots.play(20)
+console.log(Boots.loneliness)
+console.log(Boots.happiness)
