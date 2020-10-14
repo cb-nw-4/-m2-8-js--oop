719b77fd61045f5b16deb04bd21a3530d84892ec
Sandra <sandrabess@hotmail.com>
2020-10-14 17:17:13 -0400

    ex 1 completed

--- workshop/1-Cat/exercise-1.3.js
+++ workshop/1-Cat/exercise-1.3.js
@@ -21,4 +21,81 @@
 //
 // You decide how much sleep, eat, and play affects your cat's happiness.
 
+class Cat {
+    // Add code here
+    constructor (name, breed) {
+      this.name = name;
+      this.breed = breed;
+      this.species = "cat";
+      this.tiredness = 50;
+      this.hunger = 50;
+      this.loneliness = 50;
+      this.happiness = 10;
+    }
+
+    sleep = (hours) => {
+        this.tiredness -= hours * 5;
+        this.happiness += hours * 2;
+    };
+
+    eat = (nbKibbles) => {
+        this.hunger -= Math.round(nbKibbles/5);
+        this.happiness += nbKibbles;
+    };
+
+    play = (minutes) => {
+        this.loneliness -= minutes * 3;
+        this.happiness += minutes * 5;
+    };
+}
+ 
+
+  let boots = new Cat("Boots", "Simaese");
+  console.log(boots);
+
 // B) call the different methods with appropriate values and then console.log boots to see what happened to the property values.
+boots.sleep(5);
+console.log(boots);
+/*Cat {
+    sleep: [Function: sleep],
+    eat: [Function: eat],
+    play: [Function: play],
+    name: 'Boots',
+    breed: 'Simaese',
+    species: 'cat',
+    tiredness: 25,
+    hunger: 50,
+    loneliness: 50,
+    happiness: 20
+  }*/
+
+boots.eat(100);
+console.log(boots);
+/*Cat {
+    sleep: [Function: sleep],
+    eat: [Function: eat],
+    play: [Function: play],
+    name: 'Boots',
+    breed: 'Simaese',
+    species: 'cat',
+    tiredness: 25,
+    hunger: 30,
+    loneliness: 50,
+    happiness: 120
+  }*/
+
+boots.play(10);
+console.log(boots);
+
+/*Cat {
+  sleep: [Function: sleep],
+  eat: [Function: eat],
+  play: [Function: play],
+  name: 'Boots',
+  breed: 'Simaese',
+  species: 'cat',
+  tiredness: 25,
+  hunger: 30,
+  loneliness: 20,
+  happiness: 170
+}*/
--- workshop/1-Cat/exercise-1.4.js
+++ workshop/1-Cat/exercise-1.4.js
@@ -10,4 +10,57 @@
 //
 //    It's up to you to decide how quickly these values should change.
 
+class Cat {
+    // Add code here
+    constructor (name, breed) {
+      this.name = name;
+      this.breed = breed;
+      this.species = "cat";
+      this.tiredness = 50;
+      this.hunger = 50;
+      this.loneliness = 50;
+      this.happiness = 10;
+    }
+    
+    sleep = (hours) => {
+        this.tiredness -= hours * 5;
+        this.happiness += hours * 2;
+    };
+
+    eat = (nbKibbles) => {
+        this.hunger -= Math.round(nbKibbles/5);
+        this.happiness += nbKibbles;
+    };
+
+    play = (minutes) => {
+        this.loneliness -= minutes * 3;
+        this.happiness += minutes * 5;
+    };
+
+    wait = (minutes) => {
+        this.tiredness += minutes * 5;
+        this.loneliness += minutes * 10;
+        this.hunger += minutes * 2;
+        this.happiness -= minutes * 2;
+    };
+}
+ 
+
+  let boots = new Cat("Boots", "Simaese");
+
 // B) Make Boots wait 20 minutes and call then console.log(boots);
+boots.wait(20);
+console.log(boots);
+/*Cat {
+    sleep: [Function: sleep],
+    eat: [Function: eat],
+    play: [Function: play],
+    wait: [Function: wait],
+    name: 'Boots',
+    breed: 'Simaese',
+    species: 'cat',
+    tiredness: 150,
+    hunger: 90,
+    loneliness: 250,
+    happiness: -30
+  }*/
