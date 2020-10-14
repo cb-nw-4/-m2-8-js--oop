49385254545b910b02686531c06da73f7886d58e
Feriel Nabet <fnabet@ipconnex.com>
2020-10-13 21:50:58 -0400

    Answers exercise 1

--- workshop/1-Cat/exercise-1.1.js
+++ workshop/1-Cat/exercise-1.1.js
@@ -14,11 +14,24 @@
 // }
 
 class Cat {
-  // Add code here
+    // Add code here
+    constructor() {
+        this.species = 'cat';
+        this.tiredness = 0;
+        this.hunger = 0;
+        this.loneliness = 0;
+        this.happiness = 0;
+    }
 }
 
 // B) Instantiate a cat called 'boots' with the Cat class.
 
+let boots = new Cat();
+console.log(boots);
+
 // C) What do you see when you console.log(boots)?
+//we will have object cat with species, tiredness, hunger, loneliness, hapiness as keys of the object
+
 
 // D) What if I want to output just boots' species?
+boots.species;
