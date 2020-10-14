a2d8b0d453854ac0f6600d7a590d1be7016f2e20
Feriel Nabet <fnabet@ipconnex.com>
2020-10-13 21:50:43 -0400

    update samples part

--- samples/sample-5.js
+++ samples/sample-5.js
@@ -1,15 +1,20 @@
 class Dog {
-  constructor() {
-    this.hunger = 0;
-  }
+    constructor() {
+        this.hunger = 0;
+    }
 
-  run = () => {
-    this.eat();
-  };
 
-  food = () => {
-    this.hunger = this.hunger - 10;
-  };
+    eat = () => {
+        this.hunger = this.hunger + 10;
+    }
+
+    run = () => {
+        this.eat();
+    };
+
+    food = () => {
+        this.hunger = this.hunger - 10;
+    };
 }
 
 let fido = new Dog();
