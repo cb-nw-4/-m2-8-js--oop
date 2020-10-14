f36f9253f7f8d7bf34ab6fbc85d621e76a5b9b14
chris shino <chris.shino@hotmail.com>
2020-10-13 21:44:31 -0400

    done 2.3

--- workshop/2-Books/exercise-2.3.js
+++ workshop/2-Books/exercise-2.3.js
@@ -2,36 +2,55 @@
 // Copy over your solutions classes you created in 2.1 and 2.2.
 // Paste them right here:
 
-
 class Book {
-  constructor (title, genre, author, isRead=false) {
-    this.title = title
-    this.genre = genre
-    this.author = author
-    this.isRead = isRead
+  constructor(title, genre, author, isRead = false) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.isRead = isRead;
   }
 }
 
-book1 = new Book('Harry Potter 1', 'Fantasy', 'J.K. Rowling')
-book2 = new Book('Harry Potter 1', 'Fantasy', 'J.K. Rowling', true)
-book3 = new Book('Harry Potter 1', 'Fantasy', 'J.K. Rowling', false)
-book4 = new Book('Harry Potter 1', 'Fantasy', 'J.K. Rowling', false)
-book5 = new Book('Harry Potter 1', 'Fantasy', 'J.K. Rowling', false)
+book1 = new Book("Harry Potter 1", "Fantasy", "J.K. Rowling", true);
+book2 = new Book("Harry Potter 1", "Fantasy", "J.K. Rowling", true);
+book3 = new Book("Harry Potter 1", "Fantasy", "J.K. Rowling", false);
+book4 = new Book("Harry Potter 1", "Fantasy", "J.K. Rowling", false);
+book5 = new Book("Harry Potter 1", "Fantasy", "J.K. Rowling", false);
 
 class BookList {
   // Code here
-  constructor () {
-    this.books = []
-    this.lastRead = this.books.length[this.books.length - 1]
-    this.currentlyReading = this.books.length
+  constructor() {
+    this.books = [];
+    this.lastRead = null;
+    this.currentlyReading = null;
+  }
+  add = (book) => {
+    this.books = this.books.concat(book);
+    if (this.currentlyReading === null) {
+      this.currentlyReading = book;
+    }
+  };
+
+  getNumRead = () => {
+    let count = 0
+    this.books.forEach(book => {
+      if (book.isRead === true){
+        count = count + 1
+      }
+    })
+    return count 
+  }
+
+  getNumUnread = () => {
+    let count = 0
+    this.books.forEach(book => {
+      if (book.isRead === false){
+        count = count + 1
+      }
+    })
+    return count 
   }
 }
-
-let homeLibrary = new Booklist()
-
-
-
-
 
 // Exercise 2.3
 //
@@ -56,12 +75,12 @@ let homeLibrary = new Booklist()
 const homeLibrary = new BookList();
 
 // Books are unread by default:
-homeLibrary.add(new Book('The Shining', 'Horror', 'Stephen King'));
-homeLibrary.add(new Book('American Gods', 'Fiction', 'Neil Gaiman'));
+homeLibrary.add(new Book("The Shining", "Horror", "Stephen King"));
+homeLibrary.add(new Book("American Gods", "Fiction", "Neil Gaiman"));
 
 // But, we can specify that we've read it:
 homeLibrary.add(
-  new Book('Eloquent JavaScript', 'Programming', 'Marijn Haverbeke', true)
+  new Book("Eloquent JavaScript", "Programming", "Marijn Haverbeke", true)
 );
 
 // At this point, we should have 2 unread books, and 1 read book:
