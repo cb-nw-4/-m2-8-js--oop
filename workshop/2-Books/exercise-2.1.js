c43fa181ff634dc3f9e8da9e65f90de10f9acb81
Doug Wright <dw@nof49.ca>
2020-10-14 16:16:37 -0400

    Completed exercise 2.1

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,72 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+    constructor(title, genre, author, isRead) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.isRead = false;
+    }
+}
+
+book1 = new Book('JFK: Coming of Age in the American Century, 1917-1956',
+    'Biograghy',
+    'Fredrik Logevall',
+    false);
+
+book2 = new Book('To Sleep in a Sea of Stars',
+    'Science Fiction',
+    'Christopher Paolini',
+    false);
+
+book3 = new Book('Eat a Peach',
+    'Business',
+    'David Chang');
+
+book4 = new Book('A Rogue of One\'s Own',
+    'Romance',
+    'Evie Dunmore',
+    false);
+
+book5 = new Book('When No One is Watching',
+    'Mystery',
+    'Alyssa Cole');
 
 console.log(book1, book2, book3, book4, book5);
+
+// console.log output:
+Book {
+    title: 'JFK: Coming of Age in the American Century, 1917-1956',
+    genre: 'Biograghy',
+    author: 'Fredrik Logevall',
+    isRead: false
+}
+
+Book {
+    title: 'To Sleep in a Sea of Stars',
+    genre: 'Science Fiction',
+    author: 'Christopher Paolini',
+    isRead: false
+}
+
+Book {
+    title: 'Eat a Peach',
+    genre: 'Business',
+    author: 'David Chang',
+    isRead: false
+}
+
+Book {
+    title: "A Rogue of One's Own",
+    genre: 'Romance',
+    author: 'Evie Dunmore',
+    isRead: false
+}
+
+Book {
+    title: 'When No One is Watching',
+    genre: 'Mystery',
+    author: 'Alyssa Cole',
+    isRead: false
+}
