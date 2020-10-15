c79c65285ff9811abd4afd7120654911d8818fea
Gia Dat Doan <giadatdoan@gmail.com>
2020-10-15 18:52:36 -0400

    2.3 - done

--- workshop/2-Books/exercise-2.3.js
+++ workshop/2-Books/exercise-2.3.js
@@ -1,6 +1,50 @@
 // From 2.1. and 2.2
 // Copy over your solutions classes you created in 2.1 and 2.2.
 // Paste them right here:
+//let bookRead = 0;
+let bookNotRead = 0;
+
+class Book {
+  constructor(title, genre, author, isRead = false) {
+      this.title = title;
+      this.genre = genre;
+      this.author = author;
+      this.isRead = isRead;
+  }
+}
+
+class BookList {
+  constructor() {
+    this.books = [];
+    this.lastRead = null;
+    this.currentlyReading = null;
+  };
+
+    add = (book) => {
+      this.books.push(book);
+      this.currentlyReading = book;
+    }
+
+    getNumRead = () => {
+      let bookRead = 0;
+      this.books.forEach((book) => {
+        if (book.isRead === true) {
+          bookRead += 1;
+        }
+      })
+      return bookRead;
+    }
+
+    getNumUnread = () => {
+      let bookUnread = 0;
+      this.books.forEach((book) => {
+        if (book.isRead === false) {
+          bookUnread += 1;
+        }
+      })
+      return bookUnread;
+    }
+}
 
 // Exercise 2.3
 //
@@ -22,6 +66,8 @@
 //
 // The following code will fail by default. Your goal is to get it to run, and output the values specified at the end:
 
+
+//UNCOMMENT LATER
 const homeLibrary = new BookList();
 
 // Books are unread by default:
