858617674963e3eb44bb5dc7d28e777928f1b143
Mallory Belgica <mallorybelgica@gmail.com>
2020-10-15 20:30:33 -0400

    Completed Cat exercise

--- workshop/1-Cat/exercise-1.2.js
+++ workshop/1-Cat/exercise-1.2.js
@@ -9,3 +9,23 @@
 // and confirm that you have everything in there.
 
 // By the way, Boots is a Simaese cat...
+
+class Cat {
+    // Add code here
+    constructor(name, breed) {
+    this.name = name;
+    this.species = 'cat';
+    this.breed = breed;
+    this.tiredness = 0;
+    this.hunger = 0;
+    this.loneliness = 0;
+    this.happiness = 0;
+    }
+
+}
+
+let boots = new Cat('Boots', 'Siamese cat');
+
+console.log(boots);
+
+
