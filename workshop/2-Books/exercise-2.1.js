13d5252b548c716375c5082ef74aee091f6ea36f
Mallory Belgica <mallorybelgica@gmail.com>
2020-10-17 15:21:02 -0400

    Completed Books exercise

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,19 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+    constructor(title, genre, author, isRead) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.isRead = isRead || false;
+    }
+}
+
+let book1 = new Book("1984", "Political Fiction", "George Orwell", true);
+let book2 = new Book("The Sun Also Rises", "Fiction", "Ernest Hemingway", true);
+let book3 = new Book("Kitchen Confidential", "Memoir", "Anthony Bourdain", false);
+let book4 = new Book("2001: a Space Odyssey", "Science Fiction", "Arthur C. Clarke", false);
+let book5 = new Book("Becoming", "Memoir", "Michelle Obama", true)
 
 console.log(book1, book2, book3, book4, book5);
