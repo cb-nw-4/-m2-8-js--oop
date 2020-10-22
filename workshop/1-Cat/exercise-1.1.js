09a47021a2b034f3166ab5d33e076ce0bdfc5645
“Winnie <chanwinnie.wc@gmail.com>
2020-10-21 23:26:39 -0400

    Ex-1

--- workshop/1-Cat/exercise-1.1.js
+++ workshop/1-Cat/exercise-1.1.js
@@ -15,10 +15,19 @@
 
 class Cat {
   // Add code here
+  constructor() {
+    this.species = "cat";
+    this.tiredness = 10;
+    this.hunger = 50;
+    this.loneliness = 0;
+    this.happiness = 75;
+
+  }
 }
 
 // B) Instantiate a cat called 'boots' with the Cat class.
-
+let boots = new Cat();
 // C) What do you see when you console.log(boots)?
-
+console.log(boots); //{species: "cat", tiredness: 10, hunger: 50, loneliness: 0, happiness: 75}
 // D) What if I want to output just boots' species?
+boots.species; //"cat"
--- workshop/1-Cat/exercise-1.3.js
+++ workshop/1-Cat/exercise-1.3.js
@@ -8,7 +8,31 @@
 //    modify the various porperties of a cat.
 //    Write methods that will update tiredness, hunger, loneliness, and
 //    happiness.
-
+class Cat {
+    // Add code here
+    constructor(name) {
+      this.name = name;
+      this.tiredness = 100;
+      this.hunger = 70;
+      this.loneliness = 50;
+      this.happiness = 30;
+    }
+    sleep = (hours) => {
+        return this.tiredness = this.tiredness - (5 * hours);
+    }    
+    eat = (kibbles) => {
+        return this.hunger = this.hunger - (kibbles / 5);
+    }
+    play = (minutes) => {
+        return this.loneliness = this.loneliness - ((minutes/60) * 3);
+    }
+    happy = () => {
+        this.happiness += 45;
+        this.tiredness -= 20;
+        this.hunger -= 15;
+        this.loneliness -= 30;
+    }
+}
 // HINT: You need to write arrow functions here. () => {}
 // - Write a sleep method that accept hours and reduces tiredness by 5 times
 //   that number. So if the cat has a tiredness of `50`, and sleeps for 10
@@ -20,5 +44,37 @@
 // - the happiness property should be modified all of the above methods as well.
 //
 // You decide how much sleep, eat, and play affects your cat's happiness.
-
+let boots = new Cat("Boots");
+console.log(boots);
+/*
+{
+  sleep: [Function: sleep],
+  eat: [Function: eat],
+  play: [Function: play],
+  happy: [Function: happy],
+  name: 'Boots',
+  tiredness: 100,
+  hunger: 70,
+  loneliness: 50,
+  happiness: 30
+}
+*/
 // B) call the different methods with appropriate values and then console.log boots to see what happened to the property values.
+boots.sleep(5);
+boots.eat(100);
+boots.play(90); //minutes
+boots.happy();
+
+console.log(boots);
+/*{
+    sleep: [Function: sleep],
+    eat: [Function: eat],
+    play: [Function: play],
+    happy: [Function: happy],
+    name: 'Boots',
+    tiredness: 55,
+    hunger: 35,
+    loneliness: 15.5,
+    happiness: 75
+  }
+*/
